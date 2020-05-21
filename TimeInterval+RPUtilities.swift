a5b3e5d011e92f189d5e66bb0083aa82c38f3b0a
Ryan Purpura <ryan@rpurp.com>
2020-05-21 15:15:07 -0700

    Fixed missing import

--- TimeInterval+RPUtilities.swift
+++ TimeInterval+RPUtilities.swift
@@ -1,3 +1,5 @@
+import Foundation
+
 extension TimeInterval {
   var asSecondsHoursSeconds: (Int, Int, Int) { Int(self + 0.5).asSecondsHoursSeconds }
   var asFormattedTime: String { Int(self + 0.5).asFormattedTime }

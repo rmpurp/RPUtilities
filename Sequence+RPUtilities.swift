7f820e4bd46b48a4c4db43edafd6d9f5c34dd0c3
Ryan Purpura <ryan@rpurp.com>
2020-05-21 14:45:38 -0700

    Added sequence extension

---
+++ Sequence+RPUtilities.swift
@@ -0,0 +1,10 @@
+import Foundation
+
+extension Sequence {
+  func sorted<T: Comparable>(by keyPath: KeyPath<Element, T>) -> [Element] {
+    return sorted { a, b in
+      return a[keyPath: keyPath] < b[keyPath: keyPath]
+    }
+  }
+}
+

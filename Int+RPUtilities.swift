c6c777c406b3a49353cea75526c19935f3ee7ce3
Ryan Purpura <ryan@rpurp.com>
2020-05-21 22:54:01 -0700

    Fixed typos

--- Int+RPUtilities.swift
+++ Int+RPUtilities.swift
@@ -1,10 +1,10 @@
 extension Int {
-  var asSecondsHoursSeconds: (Int, Int, Int) {
+  var asHoursMinutesSeconds: (Int, Int, Int) {
     (self / 3600, (self % 3600) / 60, (self % 3600) % 60)
   }
 
   var asFormattedTime: String {
-    let (h, m, s) = self.asSecondsHoursSeconds
+    let (h, m, s) = self.asHoursMinutesSeconds
     if h == 0 {
       return String(format: "%d:%02d", m, s)
     } else {
--- TimeInterval+RPUtilities.swift
+++ TimeInterval+RPUtilities.swift
@@ -1,6 +1,6 @@
 import Foundation
 
 extension TimeInterval {
-  var asSecondsHoursSeconds: (Int, Int, Int) { Int(self + 0.5).asSecondsHoursSeconds }
+  var asHoursMinutesSeconds: (Int, Int, Int) { Int(self + 0.5).asHoursMinutesSeconds }
   var asFormattedTime: String { Int(self + 0.5).asFormattedTime }
 }
